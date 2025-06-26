af541e215602d0931a120bf54e2701126e893d7b
Vardandatasciences <vdatasciences@gmail.com>
2025-06-26 11:09:48 +0530

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   e2e: {
     supportFile: false,  // Disable the support file if not needed
-    baseUrl: 'http://localhost:3000',  // Adjust as needed
+    baseUrl: 'http://localhost:8080',  // Adjust as needed
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
   component: {
