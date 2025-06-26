3c8e26023605b289c793c0bb0979a3e75fdb0ada
Vardandatasciences <vdatasciences@gmail.com>
2025-06-26 10:23:17 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js'
+  },
+  component: {
+    devServer: {
+      framework: 'vue', // Use Vue.js framework
+      bundler: 'vite',  // Use Vite as the bundler
     },
   },
-});
+})
